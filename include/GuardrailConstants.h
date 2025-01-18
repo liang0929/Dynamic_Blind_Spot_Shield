8bb920514972d720a83c90ea6a9dff8b9e78a1ad
Bryan-Liang <laing0929@gmail.com>
2025-01-18 12:14:58 +0800

    新增main V2.1.2

---
+++ include/GuardrailConstants.h
@@ -0,0 +1,17 @@
+#ifndef FB021590_DF7E_47B9_A601_131F9A9817EF
+#define FB021590_DF7E_47B9_A601_131F9A9817EF
+
+#define FRONT_TRIG 8
+#define FRONT_ECHO 9
+#define BACK_TRIG 10
+#define BACK_ECHO 11
+#define setSensorHeight 12
+#define in1 2
+#define in2 3
+#define pwm 5
+#define switchPin 4
+
+#define enforcementMode 13
+#define THRESHOLD 35
+
+#endif /* FB021590_DF7E_47B9_A601_131F9A9817EF */
