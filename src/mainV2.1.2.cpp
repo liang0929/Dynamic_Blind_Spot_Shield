d2d40ab709d85d76930a614430dfb67d2738e223
Bryan-Liang <laing0929@gmail.com>
2025-01-20 20:44:44 +0800

    讓馬達自持&降低下降速度

--- src/mainV2.1.2.cpp
+++ src/mainV2.1.2.cpp
@@ -34,19 +34,18 @@ void loop() {
         heightControl(1);  // 執行上升
     }else
     {
-        Serial.println(isObstacleDetected);
         // 前方障礙物檢測並執行上升
         if (frontDistance > 0 && frontSensorHeight - frontDistance >= THRESHOLD && !isObstacleDetected) {
             isObstacleDetected = true;
-            heightControl(1);  // 執行上升
             Serial.println("執行上升");
+            heightControl(1);  // 執行上升
         }
 
         // 後方障礙物確認移出並執行下降
         if (isObstacleDetected && backDistance > 0 && backSensorHeight - backDistance >= THRESHOLD) {
             isObstacleDetected = false;
-            heightControl(0);  // 執行下降
             Serial.println("執行下降");
+            heightControl(0);  // 執行下降
         }
     }
     delay(100);
@@ -56,30 +55,30 @@ void heightControl(int action) {
     if (action == 1) {
         digitalWrite(in1, HIGH);
         digitalWrite(in2, LOW);
-        analogWrite(pwm, 255);
+        analogWrite(pwm, 200);
 
         // 檢查微動開關，直到觸發為止
         while (digitalRead(switchPin) == HIGH) {
+            delay(1);
         }
-        analogWrite(pwm, 0); // 停止馬達
+        Serial.println("微動開關觸發");
+        analogWrite(pwm, 30); // 停止馬達
     } 
     else if (action == 0) {
         digitalWrite(in1, LOW);
         digitalWrite(in2, HIGH);
-        analogWrite(pwm, 255);
+        analogWrite(pwm, 50);
         unsigned long startTime = millis();
         // 在下降過程中檢測前方障礙物
-        while (millis() - startTime <= 135) {
+        while (millis() - startTime < 15) {
             float frontDistance = Ulatrasound(FRONT_TRIG, FRONT_ECHO);
-            
             // 若檢測到障礙物，立即中斷下降並執行上升
             if (frontDistance > 0 && frontDistance <= THRESHOLD) {
-                Serial.println("下降中檢測到障礙物，立即上升");
                 analogWrite(pwm, 0);  // 停止下降
                 heightControl(1);     // 切換到上升
                 return;               // 結束當前的下降函式
             }
         }
-        analogWrite(pwm, 0); // 停止馬達
+        analogWrite(pwm, 30); // 停止馬達
     }
 }
